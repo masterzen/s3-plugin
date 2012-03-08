e592f41b3d8e32acf7214dbbb926447d56c6bb8b
Brice Figureau <brice@daysofwonder.com>
2012-03-08 14:32:16 +0100

    Mark AbstractS3Callable serializable
    This allows to send S3DownloadCallable and S3UploadCallable to
    a remote slave without errors.
    
    Signed-off-by: Brice Figureau <brice@daysofwonder.com>

--- src/main/java/hudson/plugins/s3/callable/AbstractS3Callable.java
+++ src/main/java/hudson/plugins/s3/callable/AbstractS3Callable.java
@@ -1,10 +1,13 @@
 package hudson.plugins.s3.callable;
 
+import java.io.Serializable;
+
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 
-public class AbstractS3Callable 
+public abstract class AbstractS3Callable implements Serializable
 {
+    private static final long serialVersionUID = 1L;
 
     private final String accessKey;
     private final String secretKey;
